e7faf5a570b0636b25bd0484683795abcf0c362e
Hyeonki Hong <hhk7734@gmail.com>
2023-07-20 22:33:06 +0900

    feat: add nats driver

---
+++ driver/nats/driver.go
@@ -0,0 +1,134 @@
+package nats
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"sync"
+
+	"github.com/cloudevents/sdk-go/v2/event"
+	"github.com/hhk7734/equeue.go"
+	"github.com/nats-io/nats.go"
+)
+
+func Open(url string, opts ...nats.Option) *natsDriver {
+	n, err := nats.Connect(url, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	js, err := n.JetStream()
+	if err != nil {
+		panic(err)
+	}
+
+	return &natsDriver{js: js}
+}
+
+var _ equeue.Driver = new(natsDriver)
+
+type natsDriver struct {
+	js nats.JetStreamContext
+}
+
+func (n *natsDriver) Client() nats.JetStreamContext {
+	return n.js
+}
+
+func (n *natsDriver) Publish(ctx context.Context, topic string, event *event.Event) error {
+	// TODO: custom subject from topic and Event
+	_, err := n.js.Publish(topic, event.Data(), nats.ExpectStream(topic))
+	return err
+}
+
+func (n *natsDriver) Consumer(topic string, subscriptionName string, maxAckPending int) (equeue.Consumer, error) {
+	if _, err := n.js.ConsumerInfo(topic, subscriptionName); err != nil {
+		return nil, err
+	}
+
+	sub, err := n.js.PullSubscribe("", subscriptionName, nats.BindStream(topic))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	return &natsConsumer{sub: sub, maxAckPending: maxAckPending, ctx: ctx, cancelCtx: cancelCtx}, nil
+}
+
+func (n *natsDriver) Close() error {
+	return nil
+}
+
+var _ equeue.Consumer = new(natsConsumer)
+
+type natsConsumer struct {
+	sub           *nats.Subscription
+	maxAckPending int
+	ctx           context.Context
+	cancelCtx     context.CancelFunc
+
+	mu   sync.Mutex
+	msgs map[*natsMessage]struct{}
+}
+
+func (n *natsConsumer) Receive() (equeue.Message, error) {
+	for {
+		select {
+		case <-n.ctx.Done():
+			return nil, equeue.ErrConsumerStopped
+		default:
+		}
+
+		nMsgs, err := n.sub.Fetch(1, nats.Context(n.ctx))
+		switch {
+		case errors.Is(err, nats.ErrTimeout):
+			continue
+		case errors.Is(err, context.Canceled):
+			return nil, equeue.ErrConsumerStopped
+		case err != nil:
+			return nil, err
+		}
+		nMsg := nMsgs[0]
+
+		event := event.New()
+		if err := json.Unmarshal(nMsg.Data, &event); err != nil {
+			return nil, err
+		}
+
+		msg := &natsMessage{msg: nMsg, event: &event}
+
+		n.mu.Lock()
+		if n.msgs == nil {
+			n.msgs = make(map[*natsMessage]struct{})
+		}
+		n.msgs[msg] = struct{}{}
+		n.mu.Unlock()
+
+		return msg, nil
+	}
+}
+
+func (n *natsConsumer) Stop() error {
+	n.cancelCtx()
+	return nil
+}
+
+var _ equeue.Message = new(natsMessage)
+
+type natsMessage struct {
+	msg   *nats.Msg
+	event *event.Event
+}
+
+func (n *natsMessage) Event() *event.Event {
+	return n.event
+}
+
+func (n *natsMessage) Ack() {
+	n.msg.Ack()
+}
+
+func (n *natsMessage) Nack() {
+	n.msg.Nak()
+}
--- go.mod
+++ go.mod
@@ -2,10 +2,22 @@ module github.com/hhk7734/equeue.go
 
 go 1.20
 
-require github.com/cloudevents/sdk-go/v2 v2.14.0
+require (
+	github.com/cloudevents/sdk-go/v2 v2.14.0
+	github.com/nats-io/nats.go v1.27.1
+)
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.20 // indirect
+	github.com/nats-io/nkeys v0.4.4 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
