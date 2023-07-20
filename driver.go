2dedba31b550574fc36805fcdba9dc901b036543
Hyeonki Hong <hhk7734@gmail.com>
2023-07-20 23:45:22 +0900

    chore: move consumer to driver

--- driver.go
+++ driver.go
@@ -11,3 +11,16 @@ type Driver interface {
 	Consumer(topic string, subscriptionName string, maxAckPending int) (Consumer, error)
 	Close() error
 }
+
+type Consumer interface {
+	// Receive blocks until a message is received or an error occurs. If Consumer is stopped,
+	// ErrConsumerStoped is returned.
+	Receive() (Message, error)
+	Stop() error
+}
+
+type Message interface {
+	Event() *cloudevents.Event
+	Ack()
+	Nack()
+}
